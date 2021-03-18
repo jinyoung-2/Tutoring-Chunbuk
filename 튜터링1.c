6108f0f3b4a706daa7c88259c75603266acf1ebe
brenda <brenda524@naver.com>
2021-03-19 01:12:10 +0900

    comment

---
+++ 튜터링1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	int rows, columns;
+	scanf_s("%d %d", &rows, &columns);
+	int** arr = (int **)malloc(sizeof(int*) * columns);
+	for (int i = 0; i < columns; i++)
+		arr[i] = (int*)malloc(sizeof(int) * rows);
+	for (int i = 0; i < columns; i++)
+		free(arr[i]);
+	free(arr);
+	return 0;
+}
+//https://codeng.tistory.com/8
+//https://m.blog.naver.com/PostView.nhn?blogId=jsky10503&logNo=221261265884&proxyReferer=https:%2F%2Fwww.google.co.kr%2F
